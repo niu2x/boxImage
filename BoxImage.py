29c69405b4b38f86f62c861ea911b74d4b863e8a
Niu2x <niu2x@aol.com>
2015-08-04 19:50:50 +0800

    p

--- BoxImage.py
+++ BoxImage.py
@@ -9,7 +9,7 @@ def box(filename, size):
 		print "can't open file"
 		return
 	filename, ext = os.path.splitext(filename)
-	outfilename = filename + '-box-' + str(size) + '.' + ext
+	outfilename = filename + '-box-' + str(size) + ext
 	for i in range(img.size[0]):
 		for j in range(img.size[1]):
 			if i % size == 0 or j % size == 0:
