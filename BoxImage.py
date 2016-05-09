734337fb8615b87732d3d8fad29381f4829749dc
niu2x <niu2x@aol.com>
2016-05-09 23:25:15 +0800

    p

--- BoxImage.py
+++ BoxImage.py
@@ -2,7 +2,7 @@ import sys
 import os
 import argparse
 from PIL import Image
-def box(filename, size):
+def box(filename, size, gray):
 	try:
 		img = Image.open(filename)
 	except IOError, e:
@@ -14,10 +14,14 @@ def box(filename, size):
 		for j in range(img.size[1]):
 			if i % size == 0 or j % size == 0:
 				img.putpixel((i, j), (0, 0, 0))
+	print gray
+	if gray == 'true':
+		img = img.convert(mode='L')
 	img.save(outfilename)
 if __name__ == "__main__":
 	parse = argparse.ArgumentParser(prog="BoxImage", description="None")
 	parse.add_argument("filename", help="None")
 	parse.add_argument("size", help="", type=int)
+	parse.add_argument("gray", help="", type=str)
 	args = parse.parse_args()
-	box(args.filename, args.size)
+	box(args.filename, args.size, args.gray)
